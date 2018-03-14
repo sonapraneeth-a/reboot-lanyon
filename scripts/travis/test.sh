1e419ad58ccf521b597507bc1a7002fd7c54bfa0
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-03-14 19:35:57 +0530

    Added some commands to install gulp on travis

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -4,6 +4,9 @@ set -x
 # bundle exec jekyll build --config _config.yml
 # bundle exec htmlproofer ./public_html --disable-external
 bundle install
+npm --version
+npm install gulp
+gulp --version
 npm install 
 gulp build::netlify::site --prod
 bundle exec htmlproofer ./.tmp/public_html --disable-external
