bf75ed361ab7f4fd7c47b7d6a4b2fa2588b6c7b5
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-08-30 12:02:17 +0530

    Created a new branch to start work on version 2 of the theme

--- scripts/travis/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-# bundle exec jekyll build --config _config.yml
-# bundle exec htmlproofer ./public_html --disable-external
-bundle install
-npm --version
-gulp build::netlify::site --prod
-bundle exec htmlproofer ./.tmp/public_html --disable-external
