93048e26c23b2764335780e6995e4858e4f3b33b
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2019-01-06 21:24:15 +0530

    Updated code to use gulp-sass intead of gulp-ruby-sass

--- gulp/tasks/gulp/build.js
+++ gulp/tasks/gulp/build.js
@@ -3,11 +3,13 @@ const argv       = require("yargs").argv;
 const gulp       = require("gulp");
 const shell      = require("shelljs");
 const log        = require("fancy-log");
-const sass       = require("gulp-ruby-sass");
+const sass       = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 
 // include paths file
 const paths = require("../../paths");
+
+sass.compiler = require("node-sass");
 
 // "gulp build::gulp-site --prod" -- Compiles the source code to html files
 gulp.task("build::gulp-site", (done) =>
@@ -39,34 +41,18 @@ gulp.task("gulp::build-sass", () => {
     /*log("=== Compiling SASS ===");*/
     if(argv.prod)
     {
-        return sass
-        (paths.temp_src_dir + "_scss/main.scss",
-            {
-                sourcemap: false,
-                style: "compressed",
-            }
-        )
-        .on("error", function (err)
-        {
-            console.error("Error!", err.message);
-        })
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.temp_site_dir + "public/css"));
+        return gulp.src(paths.temp_src_dir + "_scss/main.scss")
+                .pipe(sass.sync({outputStyle: "compressed"})
+                .on("error", sass.logError))
+                .pipe(gulp.dest(paths.temp_site_dir + "public/css"));
     }
     else
     {
-        return sass
-        (paths.temp_src_dir + "_scss/main.scss",
-            {
-                sourcemap: true,
-                style: "expanded",
-                verbose: true,
-                emitCompileError: true,
-            }
-        )
-        .on('error', sass.logError)
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.temp_site_dir + "public/css"));
+        return gulp.src(paths.temp_src_dir + "_scss/main.scss")
+                .pipe(sourcemaps.init())
+                .pipe(sass().on('error', sass.logError))
+                .pipe(sourcemaps.write())
+                .pipe(gulp.dest(paths.temp_site_dir + "public/css"));
     }
     /*done();*/
 });
