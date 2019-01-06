8d44752f7017137dbc91da8914e7b734445a31b8
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2019-01-06 10:34:56 +0530

    Renamed tasks in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,22 +12,22 @@ const tasks       = require_dir("./gulp/tasks", {recurse: true});
 const paths = require("./gulp/paths");
 
 // Jekyll tasks
-gulp.task("default::serve::jekyll", gulp.series("clean::public_html", "serve::jekyll-site"));
-gulp.task("default::serve::local::jekyll", gulp.series("clean::public_html", "serve::local::jekyll-site"));
-gulp.task("default::build::jekyll", gulp.series("clean::public_html", "build::jekyll-site"));
-gulp.task("default::jekyll", gulp.series("default::serve::jekyll"));
-gulp.task("default::local::jekyll", gulp.series("default::serve::local::jekyll"));
+gulp.task("serve::jekyll", gulp.series("clean::public_html", "serve::jekyll-site"));
+gulp.task("serve::local::jekyll", gulp.series("clean::public_html", "serve::local::jekyll-site"));
+gulp.task("build::jekyll", gulp.series("clean::public_html", "build::jekyll-site"));
+gulp.task("jekyll", gulp.series("serve::jekyll"));
+gulp.task("local::jekyll", gulp.series("serve::local::jekyll"));
 
 // Netlify tasks
-gulp.task("default::build::netlify", gulp.series("clean::public_html", "build::netlify-site"));
-gulp.task("default::serve::netlify", gulp.series("clean::public_html", "serve::netlify-site"));
+gulp.task("build::netlify", gulp.series("clean::public_html", "build::netlify-site"));
+gulp.task("serve::netlify", gulp.series("clean::public_html", "serve::netlify-site"));
 
 // Gulp tasks for better output increasing page speed
-gulp.task("default::build::gulp", gulp.series("clean::tmp", "build::netlify-site"));
-gulp.task("default::serve::gulp", gulp.series("clean::tmp", "build::netlify-site"));
-gulp.task("default::gulp", gulp.series("default::build::gulp", "default::serve::gulp"));
+gulp.task("build::gulp", gulp.series("clean::tmp", "build::netlify-site"));
+gulp.task("serve::gulp", gulp.series("clean::tmp", "build::netlify-site"));
+gulp.task("gulp", gulp.series("build::gulp", "serve::gulp"));
 
-// Default task (Either default::jekyll or default::netlify)
+// Default task (Either jekyll or netlify)
 /* gulp default : No compression/minification */
 /* gulp default --prod : Compression enabled */
-gulp.task("default", gulp.series("default::local::jekyll"));
+gulp.task("default", gulp.series("local::jekyll"));
