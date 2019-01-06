f2c3618422e8ba46df4582a98304ae131898b9f5
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2019-01-06 23:56:37 +0530

    Renamed some tasks in gulpfile.js and added new defaukts

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,12 @@ const paths = require("./gulp/paths");
 gulp.task("jekyll::clean", gulp.series("clean::sass-cache", "clean::public_html"));
 gulp.task("jekyll::build", gulp.series("jekyll::clean", "build::jekyll-site"));
 gulp.task("jekyll::serve", gulp.series("jekyll::clean", "serve::jekyll-site"));
-gulp.task("jekyll", gulp.series("jekyll::serve"));
+gulp.task("jekyll--gh-pages", gulp.series("jekyll::serve"));
 
 // Netlify tasks
 gulp.task("netlify::build", gulp.series("jekyll::clean", "build::netlify-site"));
 gulp.task("netlify::serve", gulp.series("jekyll::clean", "serve::netlify-site"));
-gulp.task("netlify", gulp.series("netlify::serve"));
+gulp.task("jekyll--netlify", gulp.series("netlify::serve"));
 
 // Gulp tasks for better output increasing page speed
 gulp.task("gulp::clean", gulp.series("clean::tmp"));
@@ -28,14 +28,19 @@ gulp.task("gulp::sass", gulp.series("gulp::copy-sass", "gulp::copy-main-sass", "
 gulp.task("gulp::css", gulp.series("gulp::copy-css", "gulp::concat-css", "gulp::compress-css"));
 gulp.task("gulp::js", gulp.series("gulp::compress-scripts"));
 gulp.task("gulp::assets", gulp.series("gulp::copy-assets", "gulp::copy-images", "gulp::compress-images"));
-gulp.task("gulp::build-gulp-config", gulp.series("copy::source", "build::gulp-site", "gulp::move-html", "gulp::compress-html"));
-gulp.task("gulp::build-gulp-optimized-site", gulp.series("gulp::clean", "gulp::build-gulp-config", "gulp::sass", "gulp::css", 
+gulp.task("gulp::build-gh-pages-config", gulp.series("copy::source", "build::gulp-site", "gulp::move-html", "gulp::compress-html"));
+gulp.task("gulp::build-gh-pages-optimized-site", gulp.series("gulp::clean", "gulp::build-gh-pages-config", "gulp::sass", "gulp::css", 
         "gulp::js", "gulp::assets", "gulp::inline-source"));
 //gulp.task("gulp::serve", gulp.series("build::netlify-site"));
-gulp.task("gulp", gulp.series("gulp::build-gulp-optimized-site", "gulp::serve"));
+gulp.task("gulp--gh-pages", gulp.series("gulp::build-gh-pages-optimized-site", "gulp::serve", "gulp::gh-pages-watch"));
 
 // Default task (Either jekyll or netlify)
 /* gulp default : No compression/minification */
 /* gulp default --prod : Compression enabled */
 /* gulp default --local : using local config (_dev-config-local.yml) yml */
-gulp.task("default", gulp.series("jekyll"));
+gulp.task("default::jekyll--gh-pages", gulp.series("jekyll--gh-pages"));
+gulp.task("default::jekyll--netlify", gulp.series("jekyll--netlify"));
+gulp.task("default::gulp--gh-pages", gulp.series("gulp--gh-pages"));
+gulp.task("default::gulp--netlify", gulp.series("gulp--gh-pages"));
+gulp.task("default", gulp.series("default::jekyll--netlify"));
+//gulp.task("default", gulp.series("default::gulp--gh-pages"));
