e8451c8757364fb36e7b9d0c7f28f3d8bd9defdf
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2019-02-20 10:10:20 +0530

    Conversion of " to ` in search blog and projects js

--- source/public/js/lunr/search-blogs.js
+++ source/public/js/lunr/search-blogs.js
@@ -33,62 +33,62 @@ let search_input = document.getElementById('blog-search');
 let result_output = document.getElementById('blog-results');
 /*let main_content = document.getElementById('main-content');*/
 let query = search_input.value.toLowerCase();
-let result_text = "";
-/*if(query == "")
+let result_text = ``;
+/*if(query == ``)
 {
-    result_text = "<p>No results found for the \"" + query + "\"</p>";
+    result_text = `<p>No results found for the "` + query + `"</p>`;
 }*/
 result_output.innerHTML = result_text;
 search_input.addEventListener('keyup', function()
     {
         let query = search_input.value.toLowerCase();
         let answer = idx.search(query);
-        result_output.innerHTML = "";
-        let result_text= "";
+        result_output.innerHTML = ``;
+        let result_text= ``;
         if(answer.length == 0)
         {
-            result_text = "<p>No results found for the \"" + query + "\"</p>";
+            result_text = `<p>No results found for the "` + query + `"</p>`;
         }
-        else if(query == "")
+        else if(query == ``)
         {
-            result_text = "";
+            result_text = ``;
         }
         else
         {
-            result_text = "<p>" + answer.length + " Result(s) found</p>";
+            result_text = `<p>` + answer.length + ` Result(s) found</p>`;
         }
         for (index = 0; index < answer.length; index++)
         {
             let ref = answer[index].ref;
             let title = posts[answer[index].ref].title;
-            let baseurl = "{{site.baseurl}}";
+            let baseurl = `{{site.baseurl}}`;
             let url = posts[answer[index].ref].url;
             let date = posts[answer[index].ref].date;
             let date_string = new Date(date).toDateString();
             let excerpt = posts[answer[index].ref].excerpt;
-            /*console.log("Ref: " + ref);
-            console.log("Title: " + title);
-            console.log("URL: " + url);
-            console.log("Date: " + date);
-            console.log("Excerpt: " + excerpt);*/
-            result_text += "<div class=\"blog-item\">"
-            result_text += "<h2 class=\"blog-title\"><a href=\"" + baseurl + url + "\">";
-            /*console.log("Baseurl: " + baseurl);
-            console.log("URL: " + url);*/
-            result_text += title+"</a></h2>";
-            /*result_text += "<p class=\"blog-date\">"+date+"</p>";*/
-            result_text += "<article class=\"blog-excerpt\">"+excerpt+"</article>";
-            result_text += "</div>";
-            result_text += "</div>";
+            /*console.log(`Ref: ` + ref);
+            console.log(`Title: ` + title);
+            console.log(`URL: ` + url);
+            console.log(`Date: ` + date);
+            console.log(`Excerpt: ` + excerpt);*/
+            result_text += `<div class="blog-item">`
+            result_text += `<h2 class="blog-title"><a href="` + baseurl + url + `">`;
+            /*console.log(`Baseurl: ` + baseurl);
+            console.log(`URL: ` + url);*/
+            result_text += title+`</a></h2>`;
+            /*result_text += `<p class="blog-date">`+date+`</p>`;*/
+            result_text += `<article class="blog-excerpt">`+excerpt+`</article>`;
+            result_text += `</div>`;
+            result_text += `</div>`;
         }
         result_output.innerHTML = result_text;
         /*if(answer.length > 0)
         {
-            main_content.style.display = "none";
+            main_content.style.display = `none`;
         }
         else
         {
-            main_content.style.display = "block";
+            main_content.style.display = `block`;
         }*/
     }
 );
--- source/public/js/lunr/search-projects.js
+++ source/public/js/lunr/search-projects.js
@@ -32,53 +32,53 @@ var idx = lunr(function ()
 let search_input = document.getElementById('project-search');
 let result_output = document.getElementById('project-results');
 let query = search_input.value.toLowerCase();
-let result_text = "";
-/*if(query == "")
+let result_text = ``;
+/*if(query == ``)
 {
-    result_text = "<p>No results found for the \"" + query + "\"</p>";
+    result_text = `<p>No results found for the "` + query + `"</p>`;
 }*/
 result_output.innerHTML = result_text;
 search_input.addEventListener('keyup', function()
     {
         let query = search_input.value.toLowerCase();
         let answer = idx.search(query);
-        result_output.innerHTML = "";
-        let result_text= "";
+        result_output.innerHTML = ``;
+        let result_text= ``;
         if(answer.length == 0)
         {
-            result_text = "<p>No results found for the \"" + query + "\"</p>";
+            result_text = `<p>No results found for the "` + query + `"</p>`;
         }
-        else if(query == "")
+        else if(query == ``)
         {
-            result_text = "";
+            result_text = ``;
         }
         else
         {
-            result_text = "<p>" + answer.length + " Result(s) found</p>";
+            result_text = `<p>` + answer.length + ` Result(s) found</p>`;
         }
         for (index = 0; index < answer.length; index++)
         {
             let ref = answer[index].ref;
             let title = projects[answer[index].ref].title;
-            let baseurl = "{{site.baseurl}}";
+            let baseurl = `{{site.baseurl}}`;
             let url = projects[answer[index].ref].url;
             let date = projects[answer[index].ref].date;
             let date_string = new Date(date).toDateString();
             let brief = projects[answer[index].ref].brief;
-            /*console.log("Ref: " + ref);
-            console.log("Title: " + title);
-            console.log("URL: " + url);
-            console.log("Date: " + date);
-            console.log("Excerpt: " + excerpt);*/
-            result_text += "<div class=\"blog-item\">"
-            console.log("URL: "  + url);
-            console.log("Base URL: "  + baseurl);
-            result_text += "<h2 class=\"blog-title\"><a href=\"" + baseurl + url + "\">";
-            result_text += title+"</a></h2>";
-            /*result_text += "<p class=\"blog-date\">"+date+"</p>";*/
-            result_text += "<article class=\"blog-excerpt\">"+brief+"</article>";
-            result_text += "</div>";
-            result_text += "</div>";
+            /*console.log(`Ref: ` + ref);
+            console.log(`Title: ` + title);
+            console.log(`URL: ` + url);
+            console.log(`Date: ` + date);
+            console.log(`Excerpt: ` + excerpt);*/
+            result_text += `<div class="blog-item">`
+            console.log(`URL: `  + url);
+            console.log(`Base URL: `  + baseurl);
+            result_text += `<h2 class="blog-title"><a href="` + baseurl + url + `">`;
+            result_text += title+`</a></h2>`;
+            /*result_text += `<p class="blog-date">`+date+`</p>`;*/
+            result_text += `<article class="blog-excerpt">`+brief+`</article>`;
+            result_text += `</div>`;
+            result_text += `</div>`;
         }
         result_output.innerHTML = result_text;
     }
