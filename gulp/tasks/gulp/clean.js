95135b0d5676e7f17e5ba5faffc04142e660b55d
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2019-06-07 22:47:28 +0530

    Minor updates to clean and copy tasks in gulp

--- gulp/tasks/gulp/clean.js
+++ gulp/tasks/gulp/clean.js
@@ -1,4 +1,5 @@
 "use strict";
+const argv  = require("yargs").argv;
 const del   = require("del");
 const gulp  = require("gulp");
 const log   = require("fancy-log");
@@ -6,10 +7,14 @@ const log   = require("fancy-log");
 // include paths file
 const paths = require("../../paths");
 
-// "gulp clean::tmp" -- Delete the temp (.tmp/) folder
-gulp.task("clean::tmp", function(done)
+// "gulp clean::tmp" -- Delete the temp (.tmp/) folder as well as its contents
+gulp.task("clean::tmp", async () =>
 {
-    log("=== Deleting tmp folder ===");
-    del.sync([paths.temp_folder], {dryRun: false});
-    done();
+    log("=== Deleting .tmp folder ===");
+    const deletedPaths = await del([paths.temp_dir], {dryRun: false});
+    if (argv.verbose)
+    {
+        log(`Deleted files and directories:\n ${deletedPaths.join("\n")}`);
+    }
+    log("=== Deleted .tmp folder ===");
 });
--- gulp/tasks/gulp/copy.js
+++ gulp/tasks/gulp/copy.js
@@ -4,6 +4,7 @@ const argv      = require("yargs").argv;
 const size      = require("gulp-size");
 const log       = require("fancy-log");
 const rename    = require("gulp-rename");
+const when      = require("gulp-if");
 
 // include paths file
 const paths = require("../../paths");
@@ -19,94 +20,126 @@ let main_sass_file = exclude_source_dir + "public/css/main.scss";
 let main_gulp_sass_file = exclude_source_dir + "public/css/main-gulp.scss";
 
 // "gulp copy:source" --
-gulp.task("copy::source", () =>
+gulp.task("copy::source", async () =>
 {
-    /*log("=== Copying source files to tmp folder ===");*/
-    return gulp.src([paths.source_dir + "**/*", 
+    // Reference: https://stackoverflow.com/questions/34188287/how-to-use-async-await-with-gulp-4
+    let source = [paths.source_dir + "**/*",
                     assets_contents, assets_folder,
                     sass_contents, sass_folder, main_gulp_sass_file, main_sass_file,
-                    css_files, images])
-        .pipe(
-            size
-            ({
-                title: "Copying:",
-                showFiles: true,
-                pretty: true
-            })
-        )
-        .pipe(gulp.dest(paths.temp_dir + paths.source_folder))
-    /*done();*/
+                    css_files, images];
+    let dest = paths.temp_dir + paths.source_folder;
+    await new Promise((resolve, reject) => {
+        log(`=== Copying source files to ${dest} folder ===`);
+        gulp.src(source)
+            .pipe(
+                when(argv.verbose,
+                    size({
+                        title: "Copying:",
+                        showFiles: true,
+                        pretty: true
+                    })
+                )
+            )
+            .pipe(gulp.dest(dest))
+            .on("finish", function() {
+                return resolve(log(`=== Copied source files to ${dest} folder ===`));
+            });
+    });
 });
 
-gulp.task("gulp::copy-sass", () =>
+gulp.task("gulp::copy-sass", async () =>
 {
-    /*log("=== Copying SCSS files ===");*/
-    return gulp.src([paths.source_dir + "_sass" + paths.scss_pattern])
+    await new Promise((resolve, reject) => {
+        log(`=== Copying SCSS files to ${paths.temp_src_dir + paths.sass_folder + "/"} ===`);
+        gulp.src([paths.source_dir + paths.sass_folder + paths.scss_pattern])
         .pipe(
-            size
-            ({
-                title: "Copying:",
-                showFiles: true,
-                pretty: true
-            })
+            when(argv.verbose,
+                size({
+                    title: "Copying:",
+                    showFiles: true,
+                    pretty: true
+                })
+            )
         )
-        .pipe(gulp.dest(paths.temp_src_dir + "_sass/"))
-    /*done();*/
+        .pipe(gulp.dest(paths.temp_src_dir + paths.sass_folder + "/"))
+        .on("finish", function() {
+            return resolve(log(`=== Copied SCSS files to ${paths.temp_src_dir + paths.sass_folder + "/"} folder ===`));
+        });
+    });
 });
 
-gulp.task("gulp::copy-main-sass", () => 
+gulp.task("gulp::copy-main-sass", async () =>
 {
-    /*log("=== Moving Main CSS file ===");*/
-    return gulp.src([paths.temp_site_dir + "public/css/main.scss.txt"])
+    await new Promise((resolve, reject) => {
+        log("=== Moving Main SCSS file ===");
+        gulp.src([paths.temp_site_dir + "public/css/main.scss.txt"])
         .pipe(rename("main.scss"))
         .pipe(gulp.dest(paths.temp_src_dir + "_sass/"))
-    /*done();*/
+        .on("finish", function() {
+            return resolve(log("=== Copied Main SCSS file to .tmp folder ==="));
+        });
+    });
 })
 
-gulp.task("gulp::copy-css", () =>
+gulp.task("gulp::copy-css", async () =>
 {
-    /*log("=== Copying CSS files ===");*/
-    return gulp.src([paths.source_dir + "public" + paths.css_pattern])
+    await new Promise((resolve, reject) => {
+        log("=== Copying CSS files ===");
+        gulp.src([paths.source_dir + "public" + paths.css_pattern])
         .pipe(
-            size
-            ({
-                title: "Copying:",
-                showFiles: true,
-                pretty: true
-            })
+            when(argv.verbose,
+                size({
+                    title: "Copying:",
+                    showFiles: true,
+                    pretty: true
+                })
+            )
         )
         .pipe(gulp.dest(paths.temp_site_dir + "public"))
-    /*done();*/
+        .on("finish", function() {
+            return resolve(log("=== Copied CSS files to .tmp folder ==="));
+        });
+    });
 });
 
-gulp.task("gulp::copy-assets", () =>
+gulp.task("gulp::copy-assets", async () =>
 {
-    /*log("=== Copying assets files to tmp folder ===");*/
-    return gulp.src([paths.source_dir + "assets/**/*"])
+    await new Promise((resolve, reject) => {
+        log("=== Copying assets files to tmp folder ===");
+        gulp.src([paths.source_dir + "assets/**/*"])
         .pipe(
-            size
-            ({
-                title: "Copying:",
-                showFiles: true,
-                pretty: true
-            })
+            when(argv.verbose,
+                size({
+                    title: "Copying:",
+                    showFiles: true,
+                    pretty: true
+                })
+            )
         )
         .pipe(gulp.dest(paths.temp_site_dir + "assets/"))
-    /*done();*/
+        .on("finish", function() {
+            return resolve(log("=== Copied source files to .tmp folder ==="));
+        });
+    });
 });
 
-gulp.task("gulp::copy-images", () =>
+gulp.task("gulp::copy-images", async () =>
 {
-    /*log("=== Copying JS files /*===");*/
-    return gulp.src([paths.source_dir + "public" + paths.image_pattern])
+    await new Promise((resolve, reject) => {
+        log("=== Copying image files ===");
+        gulp.src([paths.source_dir + "public" + paths.image_pattern])
         .pipe(
-            size
-            ({
-                title: "Copying:",
-                showFiles: true,
-                pretty: true
-            })
+            when(argv.verbose,
+                size({
+                    title: "Copying:",
+                    showFiles: true,
+                    pretty: true
+                })
+            )
         )
         .pipe(gulp.dest(paths.temp_site_dir + "public"))
-    /*done();*/
+        .on("finish", function() {
+            return resolve(log("=== Copied source files to .tmp folder ==="));
+        });
+    });
 });
